45db60fba7765315a3dfc0b28b370108466806b9
Renzenar <118459019+Renzenar@users.noreply.github.com>
2024-09-22 15:05:13 -0500

    api endpoint

--- pet-adoption-frontend/src/pages/login.jsx
+++ pet-adoption-frontend/src/pages/login.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import Head from 'next/head';
 import { useRouter } from "next/router";
 import { Button, Card, CardContent, Stack, Typography } from '@mui/material'
+import { useState } from "react";
+import { logging } from "../../next.config";
 
 export default function LoginPage() {
     const router = useRouter();
+
+    const [testShow, setTestShow] = useState("This is a test button")
+
+    const testEndpoint = () =>{
+        const url = "http://localhost:8080/test"; //Just specific for this test endpoint
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.text(); //Just using plain text for this simple test
+            })
+            .then(data => {
+                setTestShow(data); 
+            })
+            .catch(error => console.error("Error fetching test data:", error));
+    };
 
     return (
         <>
@@ -20,6 +39,9 @@ export default function LoginPage() {
                         <Typography variant='body1' color='text.secondary'>This is where a User, either owner or center, can login.</Typography>
                     </CardContent>
                 </Card>
+            </Stack>
+            <Stack direction="column">
+                <Button variant='contained' onClick={testEndpoint}>{testShow} </Button>
             </Stack>
             </main>
         </>
