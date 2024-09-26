b34060626b0d3760a3cae4b982df29fb2f3a5fa9
Renzenar <118459019+Renzenar@users.noreply.github.com>
2024-09-26 16:45:34 -0500

    Added button to change between user types

--- pet-adoption-frontend/src/pages/register.jsx
+++ pet-adoption-frontend/src/pages/register.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Head from 'next/head';
 import { useRouter } from "next/navigation";
-import { Button, Card, CardContent, Stack, Typography } from '@mui/material'
+import { Button, Card, CardContent, Stack, Typography, ButtonGroup, ToggleButtonGroup, ToggleButton } from '@mui/material'
 
-export default function ReisterPage() {
+export default function RegisterPage() {
     const router = useRouter();
+    const [alignment, setAlignment] = React.useState('owner');
+
+    const handleChange = (event, newAlignment) => {
+        if (newAlignment !== null) {
+            setAlignment(newAlignment);
+          }
+    };
 
     return (
         <>
@@ -18,6 +25,16 @@ export default function ReisterPage() {
                     <CardContent>
                         <Typography variant='h3' align='center'>Register Page</Typography>
                         <Typography variant='body1' color='text.secondary'>This is where a User, either owner or center, can register a new account.</Typography>
+                        <ToggleButtonGroup
+                            color="primary"
+                            value={alignment}
+                            exclusive
+                            onChange={handleChange}
+                            aria-label="Platform"
+                            >
+                                <ToggleButton value="owner">Potential Owner</ToggleButton>
+                                <ToggleButton value="center">Adoption Center</ToggleButton>
+                        </ToggleButtonGroup>
                     </CardContent>
                 </Card>
             </Stack>
