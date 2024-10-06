03651305d950559726765e7e478265374b6ba82c
Renzenar <118459019+Renzenar@users.noreply.github.com>
2024-10-06 16:35:47 -0500

    Removed import

--- pet-adoption-api/src/test/java/petadoption/api/user/UserTests.java
+++ pet-adoption-api/src/test/java/petadoption/api/user/UserTests.java
@@ -1,7 +1,6 @@
 package petadoption.api.user;
 
 import jakarta.transaction.Transactional;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
