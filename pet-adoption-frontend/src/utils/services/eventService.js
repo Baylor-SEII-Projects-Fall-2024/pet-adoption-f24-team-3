b97bcb2d944f13ad9851c2c3b38930b7457e2c83
Renzenar <118459019+Renzenar@users.noreply.github.com>
2024-10-14 21:52:24 -0500

    Create eventService.js

---
+++ pet-adoption-frontend/src/utils/services/eventService.js
@@ -0,0 +1,90 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentUserId } from '@/utils/redux';
+import Cookies from 'js-cookie';
+
+const eventService = () => {
+    const dispatch = useDispatch();
+    const currentUserId = useSelector((state) => state.currentUser.currentUserId);
+
+    const createEvent = async (formData) => {
+        const response = await fetch("http://localhost:8080/api/events", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                centerId: formData.centerId,
+                datePosted: formData.datePosted,
+                name: formData.name,
+                description: formData.description,
+                dateStart: formData.dateStart,
+                dateEnd: formData.dateEnd,
+                thumbnailPath: formData.thumbNail
+            })
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            // saveCurrentUserToRedux(result.userid); Don't think these are needed
+            // setAuthenticationCookies(result.userid);
+            return result;
+        } else {
+            alert(`Event creation failed: ${result.message}`);
+            return null;
+        }
+    };
+
+    const getEventInfo = async (eventID) => {
+        const response = await fetch(`http://localhost:8080/api/events/${eventID}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            return result;
+        } else {
+            alert(`Get info failed: ${result.message}`);
+            return null;
+        }
+    };
+
+    const updateEvent = async (formData, eventID) => {
+        const response = await fetch(`http://localhost:8080/api/events/update/${eventID}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                centerId: formData.centerId,
+                datePosted: formData.datePosted,
+                name: formData.name,
+                description: formData.description,
+                dateStart: formData.dateStart,
+                dateEnd: formData.dateEnd,
+                thumbnailPath: formData.thumbNail
+            })
+        });
+
+        const result = await response.json();
+        console.log("result:", result);
+        if (response.ok) {
+            //saveCurrentUserToRedux(result.eventID);
+            return result;
+        } else {
+            alert(`Update failed: ${result.message}`);
+            return null;
+        }
+    };
+
+
+    return {
+        createEvent,
+        getEventInfo,
+        updateEvent
+    };
+
+};
+export default eventService;
