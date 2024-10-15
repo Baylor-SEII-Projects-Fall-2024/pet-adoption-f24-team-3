a0061b8998ce9925d6eeb3ae7c61cb84ab380aed
Jarpletz <rothpletz5@gmail.com>
2024-10-15 16:19:49 -0500

    image uploads for edit center

--- pet-adoption-frontend/src/pages/centers/[centerId]/edit.jsx
+++ pet-adoption-frontend/src/pages/centers/[centerId]/edit.jsx
@@ -16,51 +16,62 @@ export default function EditProfilePage() {
   const [loading, setLoading] = useState(true);
   const [userInfo, setUserInfo] = useState(null);
   const [error, setError] = useState(null);
+  const [profileImage, setProfileImage] = useState(null);
+  const [bannerImage, setBannerImage] = useState(null);
   const [formData, setFormData] = useState({
     accountType: "",
     emailAddress: "",
     password: "",
     profilePicPath: null,
     name: "",
+    description: "",
     address: "",
     city: "",
     state: "",
     zipCode: ""
   });
-  
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData(prevState => ({ ...prevState, [name]: value }));
   };
+
+  const handleProfileImageUpload = (e) => {
+    setProfileImage(e.target.files[0]);
+  }
+  const handleBannerImageUpload = (e) => {
+    setBannerImage(e.target.files[0]);
+  }
+
   //handle what happens on sumbmit. Does not reroute on success.
   const handleSubmit = async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  try {
-      await updateCenter(formData,centerId)
-          .then((centerId) => {
-              //if user id is not null, that is handled in the hook below
-              if (centerId !== null) {
-                let elm = document.getElementById("errorLabel");
-                elm.innerHTML = "Profile Settings Saved!";
-                elm.style = "color: green;"
-              }
-              else {
-                let elm = document.getElementById("errorLabel");
-                elm.innerHTML = "An error occured, try again!";
-                elm.style = "color: red;"
-              }
-          })
+    try {
+      await updateCenter(formData, profileImage, bannerImage, centerId)
+        .then((centerId) => {
+          //if user id is not null, that is handled in the hook below
+          if (centerId !== null) {
+            let elm = document.getElementById("errorLabel");
+            elm.innerHTML = "Profile Settings Saved!";
+            elm.style = "color: green;"
+          }
+          else {
+            let elm = document.getElementById("errorLabel");
+            elm.innerHTML = "An error occured, try again!";
+            elm.style = "color: red;"
+          }
+        })
 
 
-  } catch (error) {
+    } catch (error) {
       console.error("Error: ", error);
       alert("An error occured saving data.");
-  }
-};
+    }
+  };
   useEffect(() => {
-    if(centerId && centerId != currentUserId){
+    if (centerId && centerId != currentUserId) {
       router.push(`/centers/${centerId}`);
     }
     if (centerId) {
@@ -73,6 +84,7 @@ export default function EditProfilePage() {
             setFormData(prevState => ({ ...prevState, ["accountType"]: result.accountType }));
             setFormData(prevState => ({ ...prevState, ["emailAddress"]: result.emailAddress }));
             setFormData(prevState => ({ ...prevState, ["name"]: result.name }));
+            setFormData(prevState => ({ ...prevState, ["description"]: result.description }));
             setFormData(prevState => ({ ...prevState, ["address"]: result.address }));
             setFormData(prevState => ({ ...prevState, ["city"]: result.city }));
             setFormData(prevState => ({ ...prevState, ["state"]: result.state }));
@@ -88,41 +100,60 @@ export default function EditProfilePage() {
       fetchUserInfo();
     }
   }, [centerId]); // rerender if userId changes
-if(!loading){
-return(
-    <>
-      <Head>
-        <title>Edit Profile Page</title>
-      </Head>
+  if (!loading) {
+    return (
+      <>
+        <Head>
+          <title>Edit Profile Page</title>
+        </Head>
 
-      <main>
-        <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
-          <Card sx={{ width: 600 }} elevation={4}>
-            <CardContent> 
-              <Typography variant='h3' align='center'>Profile Settings</Typography>
-            </CardContent>
-          </Card>
-          <Stack direction="column" >
-            <Card sx={{ minWidth:"60vw", p:"15px" }} >
-            <form id="Form" onSubmit={handleSubmit}>
-              <TextField required fullWidth label='Email' name="emailAddress" size="small" margin="dense" value={formData.emailAddress} onChange={handleChange} />
-              <TextField required fullWidth label='Name' name="name" size="small" margin="dense" value={formData.name} onChange={handleChange} />
-              <TextField required fullWidth label='Address' name="address" size="small" margin="dense" value={formData.address} onChange={handleChange} />
-              <TextField required fullWidth label='City' name="city" size="small" margin="dense" value={formData.city} onChange={handleChange} />
-              <TextField required fullWidth label='State' name="state" size="small" margin="dense" value={formData.state} onChange={handleChange} />
-              <TextField required fullWidth label='Zip Code' name="zipCode" size="small" margin="dense" value={formData.zipCode} onChange={handleChange} />
-                
-              <Button type='submit' variant='contained' color='primary'>Save</Button>
-              
-            </form>
-            <label id="errorLabel"></label>
-
+        <main>
+          <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
+            <Card sx={{ width: 600 }} elevation={4}>
+              <CardContent>
+                <Typography variant='h3' align='center'>Profile Settings</Typography>
+              </CardContent>
             </Card>
-          </Stack>
-        </Stack>
-      </main>
+            <Stack direction="column" >
+              <Card sx={{ width: "60vw", p: "15px" }} >
+                <form id="Form" onSubmit={handleSubmit}>
+                  <TextField required fullWidth label='Email' name="emailAddress" size="small" margin="dense" value={formData.emailAddress} onChange={handleChange} />
+                  <TextField required fullWidth label='Name' name="name" size="small" margin="dense" value={formData.name} onChange={handleChange} />
+                  <TextField required fullWidth multiline label='Descripion' name="description" size="small" margin="dense" value={formData.description} onChange={handleChange} />
 
-    </>
-  );  
-} 
+                  <TextField required fullWidth label='Address' name="address" size="small" margin="dense" value={formData.address} onChange={handleChange} />
+                  <TextField required fullWidth label='City' name="city" size="small" margin="dense" value={formData.city} onChange={handleChange} />
+                  <TextField required fullWidth label='State' name="state" size="small" margin="dense" value={formData.state} onChange={handleChange} />
+                  <TextField required fullWidth label='Zip Code' name="zipCode" size="small" margin="dense" value={formData.zipCode} onChange={handleChange} />
+                  <TextField
+                    type="file"
+                    label='Profile Picture'
+                    name="profilePicture"
+                    fullWidth
+                    size="small" margin="dense"
+                    InputLabelProps={{ shrink: true }}
+                    inputProps={{ accept: "image/png, image/gif, image/jpeg" }}
+                    onChange={handleProfileImageUpload} />
+                  <TextField
+                    type="file"
+                    label='Banner Image'
+                    name="bannerImage"
+                    fullWidth
+                    size="small" margin="dense"
+                    InputLabelProps={{ shrink: true }}
+                    inputProps={{ accept: "image/png, image/gif, image/jpeg" }}
+                    onChange={handleBannerImageUpload} />
+                  <Button type='submit' variant='contained' color='primary'>Save</Button>
+
+                </form>
+                <label id="errorLabel"></label>
+
+              </Card>
+            </Stack>
+          </Stack>
+        </main>
+
+      </>
+    );
+  }
 }
