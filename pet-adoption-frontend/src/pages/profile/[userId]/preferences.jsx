9d1e412da2f2ce8eb39f020bc85251765fd59527
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-10-15 11:51:31 -0500

    front-end for edit preferences

--- pet-adoption-frontend/src/pages/profile/[userId]/preferences.jsx
+++ pet-adoption-frontend/src/pages/profile/[userId]/preferences.jsx
@@ -1,33 +1,268 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { Button, Card, CardContent, Stack, Typography } from '@mui/material'
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  Typography,
+  TextField
+} from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import userService from "@/utils/services/userService";
 
 export default function EditPreferencesPage() {
   const router = useRouter();
   const { userId } = router.query; //get user ID from the routing
+  const currentUserId = useSelector((state) => state.currentUser.currentUserId); // get the current session user
+  const { updatePreferences, getPreferences } = userService();
 
-  return (
-    <>
-      <Head>
-        <title>Edit Profile Page</title>
-      </Head>
+  const [loading, setLoading] = useState(true);
+  const [preferences, setPreferences] = useState(null);
+  const [formError, setFormError] = useState(null);
+  const [formSuccess, setFormSuccess] = useState();
+  const [formData, setFormData] = useState({
+    species: "",
+    breed: "",
+    sex: "",
+    furType: "",
+    age: "",
+    size: "",
+    city: "",
+    state: "",
+  });
 
-      <main>
-        <Stack sx={{ paddingTop: 4 }} alignItems='center' gap={2}>
-          <Card sx={{ width: 600 }} elevation={4}>
-            <CardContent>
-              <Typography variant='h3' align='center'>Edit Preferences Page</Typography>
-              <Typography variant='body1' color='text.secondary'>This is where user {userId} will be able to edit their ideal pet preferences.</Typography>
-            </CardContent>
-          </Card>
-          <Stack direction="column">
-            {/* There are multiple ways to apply styling to Material UI components. One way is using the `sx` prop: */}
-            <Button variant='contained' onClick={() => router.push(`/profile/${userId}`)} sx={{ width: 200 }}>Profile Page</Button>
-            <Button variant='contained' onClick={() => router.push('/')} sx={{ width: 200 }}>Return Home</Button>
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await updatePreferences(formData, userId).then((userId) => {
+        //if user id is not null, that is handled in the hook below
+        if (userId !== null) {
+          setFormError(null);
+          setFormSuccess("Pet Preferences Saved!");
+        } else {
+          setFormError("An error occured, try again!");
+          setFormSuccess(null);
+        }
+      });
+    } catch (error) {
+      console.error("Error: ", error);
+      setFormError("An error occured saving your data.");
+    }
+
+  };
+
+  useEffect(() => {
+    if (userId && userId != currentUserId) {
+      // TODO: bad, should not allow other people to edit preferences
+      router.push(`/profile/${userId}/preferences`);
+    }
+    if (userId) {
+      const fetchUserInfo = async () => {
+        try {
+          const result = await getOwnerInfo(userId);
+          if (result !== null) {
+            setPreferences(result);
+            setFormData((prevState) => ({
+              ...prevState,
+              ["species"]: result.species,
+            }));
+            setFormData((prevState) => ({
+              ...prevState,
+              ["breed"]: result.breed,
+            }));
+            setFormData((prevState) => ({
+              ...prevState,
+              ["sex"]: result.sex,
+            }));
+            setFormData((prevState) => ({
+              ...prevState,
+              ["furType"]: result.furType,
+            }));
+            setFormData((prevState) => ({
+              ...prevState,
+              ["age"]: result.age,
+            }));
+            setFormData((prevState) => ({
+              ...prevState,
+              ["size"]: result.size,
+            }));
+            setFormData((prevState) => ({
+              ...prevState,
+              ["city"]: result.city,
+            }));
+            setFormData((prevState) => ({
+              ...prevState,
+              ["state"]: result.state,
+            }));
+          }
+          // Set error state if not ok
+        } catch (error) {
+          setFormError(
+            `Pet preferences could not be found for user ${userId}`
+          );
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchUserInfo();
+    }
+  }, [userId]); // rerender if userId changes
+
+  if (!loading) {
+    return (
+      <>
+        <Head>
+          <title>Edit Preferences Page</title>
+        </Head>
+
+        <main>
+          <Stack sx={{ paddingTop: 4 }} alignItems="center" gap={2}>
+            <Card sx={{ width: 600 }} elevation={4}>
+              <CardContent>
+                <Typography variant="h3" align="center">
+                  Pet Preferences
+                </Typography>
+              </CardContent>
+            </Card>
+            <Stack direction="column">
+              <Card sx={{ minWidth: "60vw", p: "15px" }}>
+                <form onSubmit={handleSubmit}>
+
+                  <TextField
+                    fullWidth
+                    label="Pet Species"
+                    name="species"
+                    size="small"
+                    margin="dense"
+                    value={formData.species}
+                    onChange={handleFormChange}
+                  />
+
+                  <TextField
+                    fullWidth
+                    label="Pet Breed"
+                    name="breed"
+                    size="small"
+                    margin="dense"
+                    value={formData.breed}
+                    onChange={handleFormChange}
+                  />
+
+                  <FormControl fullWidth>
+                    <InputLabel id="sex-select-label">Pet Sex</InputLabel>
+                    <Select
+                      labelId="sex-select-label"
+                      id="sex-select"
+                      value={formData.sex}
+                      label="Pet Sex"
+                      onChange={handleFormChange}
+                    >
+                      <MenuItem value={"M"}>Male</MenuItem>
+                      <MenuItem value={"F"}>Female</MenuItem>
+                    </Select>
+                  </FormControl>
+
+                  <FormControl fullWidth>
+                    <InputLabel id="pet-fur-select-label">Pet Fur Type</InputLabel>
+                    <Select
+                      labelId="pet-fur-select-label"
+                      id="pet-fur-select"
+                      value={formData.furType}
+                      label="Pet Fur Type"
+                      onChange={handleFormChange}
+                    >
+                      <MenuItem value={"bald"}>No Hair</MenuItem>
+                      <MenuItem value={"short"}>Short Haired</MenuItem>
+                      <MenuItem value={"long"}>Long Haired</MenuItem>
+                      <MenuItem value={"hypo"}>Hypoallergenic</MenuItem>
+                    </Select>
+                  </FormControl>
+
+                  <FormControl fullWidth>
+                    <InputLabel id="age-select-label">Pet Age</InputLabel>
+                    <Select
+                      labelId="age-select-label"
+                      id="age-select"
+                      value={formData.age}
+                      label="Pet Age"
+                      onChange={handleFormChange}
+                    >
+                      <MenuItem value={"baby"}>Baby</MenuItem>
+                      <MenuItem value={"adolescent"}>Adolescent</MenuItem>
+                      <MenuItem value={"adult"}>Adult</MenuItem>
+                      <MenuItem value={"old"}>Close to Euthenasia</MenuItem>
+                    </Select>
+                  </FormControl>
+
+                  <FormControl fullWidth>
+                    <InputLabel id="size-select-label">Pet Size</InputLabel>
+                    <Select
+                      labelId="size-select-label"
+                      id="breed-select"
+                      value={formData.size}
+                      label="Pet Size"
+                      onChange={handleFormChange}
+                    >
+                      <MenuItem value={"xsmall"}>Extra Small</MenuItem>
+                      <MenuItem value={"small"}>Small</MenuItem>
+                      <MenuItem value={"medium"}>Medium</MenuItem>
+                      <MenuItem value={"large"}>Large</MenuItem>
+                      <MenuItem value={"xlarge"}>Extra Large</MenuItem>
+                    </Select>
+                  </FormControl>
+
+                  <TextField
+                    fullWidth
+                    label="Pet City"
+                    name="city"
+                    size="small"
+                    margin="dense"
+                    value={formData.city}
+                    onChange={handleFormChange}
+                  />
+
+                  <TextField
+                    fullWidth
+                    label="Pet State"
+                    name="state"
+                    size="small"
+                    margin="dense"
+                    value={formData.state}
+                    onChange={handleFormChange}
+                  />
+
+                  <br></br>
+
+                  <Button type="submit" variant="contained" color="primary">
+                    Save
+                  </Button>
+
+                </form>
+
+                {formError && (
+                  <Typography color="error">{formError}</Typography>
+                )}
+                {formSuccess && (
+                  <Typography color="success">{formSuccess}</Typography>
+                )}
+              </Card>
+            </Stack>
           </Stack>
-        </Stack>
-      </main>
-    </>
-  );
+        </main>
+      </>
+    );
+  }
 }
--- pet-adoption-frontend/src/utils/services/userService.js
+++ pet-adoption-frontend/src/utils/services/userService.js
@@ -4,232 +4,281 @@ import Cookies from 'js-cookie';
 //import { headers } from 'next/headers';
 
 const userService = () => {
-    const dispatch = useDispatch();
-    const currentUserId = useSelector((state) => state.currentUser.currentUserId);
+  const dispatch = useDispatch();
+  const currentUserId = useSelector((state) => state.currentUser.currentUserId);
 
-    //  validates the user login.Returns the user ID if successful, null otherwise
-    const validateLogin = async (email, password) => {
-        const response = await fetch("http://localhost:8080/api/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                emailAddress: email,
-                password: password,
-            })
-        });
-        const result = await response.json();
+  //  validates the user login.Returns the user ID if successful, null otherwise
+  const validateLogin = async (email, password) => {
+    const response = await fetch("http://localhost:8080/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        emailAddress: email,
+        password: password,
+      })
+    });
+    const result = await response.json();
 
-        if (response.ok) {
-            saveCurrentUserToRedux(result.userid);
-            setAuthenticationCookies(result.userid);
+    if (response.ok) {
+      saveCurrentUserToRedux(result.userid);
+      setAuthenticationCookies(result.userid);
 
-            return result.userid;
-        } else {
-            return null;
-        }
-    };
+      return result.userid;
+    } else {
+      return null;
+    }
+  };
 
 
-    // registers and logs in a new center
-    const registerCenter = async (formData) => {
-        const response = await fetch("http://localhost:8080/api/centers", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                accountType: "Center",
-                emailAddress: formData.email,
-                password: formData.password,
-                name: formData.centerName,
-                address: formData.address,
-                city: formData.city,
-                state: formData.state,
-                zipCode: formData.zip,
-                description: "A safe haven for homeless pets",
-            })
-        });
+  // registers and logs in a new center
+  const registerCenter = async (formData) => {
+    const response = await fetch("http://localhost:8080/api/centers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        accountType: "Center",
+        emailAddress: formData.email,
+        password: formData.password,
+        name: formData.centerName,
+        address: formData.address,
+        city: formData.city,
+        state: formData.state,
+        zipCode: formData.zip,
+        description: "A safe haven for homeless pets",
+      })
+    });
 
-        const result = await response.json();
-        if (response.ok) {
-            saveCurrentUserToRedux(result.userid);
-            setAuthenticationCookies(result.userid);
-            return result;
-        } else {
-            alert(`Registration failed: ${result.message}`);
-            return null;
-        }
-    };
+    const result = await response.json();
+    if (response.ok) {
+      saveCurrentUserToRedux(result.userid);
+      setAuthenticationCookies(result.userid);
+      return result;
+    } else {
+      alert(`Registration failed: ${result.message}`);
+      return null;
+    }
+  };
 
-    const registerOwner = async (formData) => {
-        const response = await fetch("http://localhost:8080/api/owners", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                accountType: "Owner",
-                emailAddress: formData.email,
-                password: formData.password,
-                nameFirst: formData.firstName,
-                nameLast: formData.lastName
-            })
-        });
+  const registerOwner = async (formData) => {
+    const response = await fetch("http://localhost:8080/api/owners", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        accountType: "Owner",
+        emailAddress: formData.email,
+        password: formData.password,
+        nameFirst: formData.firstName,
+        nameLast: formData.lastName
+      })
+    });
 
-        const result = await response.json();
-        if (response.ok) {
-            saveCurrentUserToRedux(result.userid);
-            setAuthenticationCookies(result.userid);
-            return result;
-        } else {
-            alert(`Registration failed: ${result.message}`);
-            return null;
-        }
-    };
+    const result = await response.json();
+    if (response.ok) {
+      saveCurrentUserToRedux(result.userid);
+      setAuthenticationCookies(result.userid);
+      return result;
+    } else {
+      alert(`Registration failed: ${result.message}`);
+      return null;
+    }
+  };
 
-    //fetch a few pieces of user data to retain across the session for ease of access
-    //also sets the user id in redux, which is needded to access restricted pages
-    const saveCurrentUserToRedux = async (userid) => {
-        dispatch(setCurrentUserId(userid));
-        const getSessionUserData = await fetch(`http://localhost:8080/api/users/${userid}/sessionData`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
+  //fetch a few pieces of user data to retain across the session for ease of access
+  //also sets the user id in redux, which is needded to access restricted pages
+  const saveCurrentUserToRedux = async (userid) => {
+    dispatch(setCurrentUserId(userid));
+    const getSessionUserData = await fetch(`http://localhost:8080/api/users/${userid}/sessionData`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
 
-        const result = await getSessionUserData.json();
-        if (getSessionUserData.ok) {
-            dispatch({ type: 'SET_CURRENT_USER_FULL_NAME', payload: result.userFullName });
-            dispatch({ type: 'SET_CURRENT_USER_TYPE', payload: result.accountType });
-        } else {
-            console.error("Error: Unable to fetch session data for user!")
-        }
-
-    };
-
-    const setAuthenticationCookies = async (userid) => {
-        // TODO: Add API request to generate authentication token
-        Cookies.set('userId', userid, { expires: 100 });
-        Cookies.set('authenticationToken', 1, { expires: 100 });
+    const result = await getSessionUserData.json();
+    if (getSessionUserData.ok) {
+      dispatch({ type: 'SET_CURRENT_USER_FULL_NAME', payload: result.userFullName });
+      dispatch({ type: 'SET_CURRENT_USER_TYPE', payload: result.accountType });
+    } else {
+      console.error("Error: Unable to fetch session data for user!")
     }
 
-    // validates the user using user id and authentication token stored in cookie
-    const authenticateFromCookie = async () => {
-        const userIdCookie = Cookies.get('userId');
-        const authTokenCookie = Cookies.get('authenticationToken');
+  };
 
-        //TODO: Add API request to verify Auth Token
-        if (userIdCookie && authTokenCookie) {
-            saveCurrentUserToRedux(userIdCookie);
-            return true;
-        }
-        else {
-            return false;
-        }
-    };
+  const setAuthenticationCookies = async (userid) => {
+    // TODO: Add API request to generate authentication token
+    Cookies.set('userId', userid, { expires: 100 });
+    Cookies.set('authenticationToken', 1, { expires: 100 });
+  }
 
+  // validates the user using user id and authentication token stored in cookie
+  const authenticateFromCookie = async () => {
+    const userIdCookie = Cookies.get('userId');
+    const authTokenCookie = Cookies.get('authenticationToken');
 
-    const logOut = () => {
-        //remove from redux
-        dispatch(setCurrentUserId(null));
-        dispatch({ type: 'SET_CURRENT_USER_FULL_NAME', payload: null });
-        dispatch({ type: 'SET_CURRENT_USER_TYPE', payload: null });
-        dispatch({ type: 'SET_CURRENT_USER_PROFILE_PIC_PATH', payload: null });
-        //remove cookies
-        Cookies.remove("userId");
-        Cookies.remove("authenticationToken");
-    };
-
-    const getUserInfo = async (userId) => {
-        const response = await fetch(`http://localhost:8080/api/users/?id=${userId}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            return result;
-        } else {
-            alert(`Get info failed: ${result.message}`);
-            return null;
-        }
-    };
-
-    const getOwnerInfo = async (userid) => {
-        const response = await fetch(`http://localhost:8080/api/owners/${userid}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            return result;
-        } else {
-            alert(`Get info failed: ${result.message}`);
-            return null;
-        }
-    };
-
-    const getCenterInfo = async (centerId) => {
-        const response = await fetch(`http://localhost:8080/api/centers/${centerId}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            return result;
-        } else {
-            alert(`Get center info failed: ${result.message}`);
-            return null;
-        }
+    //TODO: Add API request to verify Auth Token
+    if (userIdCookie && authTokenCookie) {
+      saveCurrentUserToRedux(userIdCookie);
+      return true;
     }
+    else {
+      return false;
+    }
+  };
 
-    const updateOwner = async (formData, userid) => {
-        const response = await fetch(`http://localhost:8080/api/update/owner/${userid}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                accountType: "Owner",
-                emailAddress: formData.emailAddress,
-                password: formData.password,
-                nameFirst: formData.nameFirst,
-                nameLast: formData.nameLast
-            })
-        });
 
-        const result = await response.json();
-        if (response.ok) {
-            saveCurrentUserToRedux(result.userid);
-            return result;
-        } else {
-            alert(`Update failed: ${result.message}`);
-            return null;
-        }
-    };
+  const logOut = () => {
+    //remove from redux
+    dispatch(setCurrentUserId(null));
+    dispatch({ type: 'SET_CURRENT_USER_FULL_NAME', payload: null });
+    dispatch({ type: 'SET_CURRENT_USER_TYPE', payload: null });
+    dispatch({ type: 'SET_CURRENT_USER_PROFILE_PIC_PATH', payload: null });
+    //remove cookies
+    Cookies.remove("userId");
+    Cookies.remove("authenticationToken");
+  };
 
-    return {
-        validateLogin,
-        registerCenter,
-        registerOwner,
-        logOut,
-        getUserInfo,
-        getOwnerInfo,
-        getCenterInfo,
-        updateOwner,
-        authenticateFromCookie,
-    };
+  const getUserInfo = async (userId) => {
+    const response = await fetch(`http://localhost:8080/api/users/?id=${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return result;
+    } else {
+      alert(`Get info failed: ${result.message}`);
+      return null;
+    }
+  };
+
+  const getPreferences = async (userId) => {
+    const response = await fetch(`http://localhost:8080/preferences/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return result;
+    } else {
+      alert(`Get info failed: ${result.message}`);
+      return null;
+    }
+  };
+
+  const getOwnerInfo = async (userid) => {
+    const response = await fetch(`http://localhost:8080/api/owners/${userid}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return result;
+    } else {
+      alert(`Get info failed: ${result.message}`);
+      return null;
+    }
+  };
+
+  const getCenterInfo = async (centerId) => {
+    const response = await fetch(`http://localhost:8080/api/centers/${centerId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return result;
+    } else {
+      alert(`Get center info failed: ${result.message}`);
+      return null;
+    }
+  }
+
+  const updateOwner = async (formData, userid) => {
+    const response = await fetch(`http://localhost:8080/api/update/owner/${userid}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        accountType: "Owner",
+        emailAddress: formData.emailAddress,
+        password: formData.password,
+        nameFirst: formData.nameFirst,
+        nameLast: formData.nameLast
+      })
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      saveCurrentUserToRedux(result.userid);
+      return result;
+    } else {
+      alert(`Update failed: ${result.message}`);
+      return null;
+    }
+  };
+
+  const updatePreferences = async (formData, userid) => {
+    const response = await fetch(`http://localhost:8080/api/update/preferences/${userid}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        species: formData.species,
+        breed: formData.breed,
+        sex: formData.sex,
+        furType: formData.furType,
+        age: formData.age,
+        size: formData.size,
+        city: formData.city,
+        state: formData.state
+      })
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      saveCurrentUserToRedux(result.userid);
+      return result;
+    } else {
+      alert(`Update failed: ${result.message}`);
+      return null;
+    }
+  };
+
+
+
+  return {
+    validateLogin,
+    registerCenter,
+    registerOwner,
+    logOut,
+    getUserInfo,
+    getPreferences,
+    getOwnerInfo,
+    getCenterInfo,
+    updateOwner,
+    updatePreferences,
+    authenticateFromCookie,
+  };
 
 };
 export default userService;
