ca71c7f210fcddd81d861f0a9e50fafe678d3056
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-10-22 16:11:21 -0500

    update gcp ip

--- pet-adoption-api/src/main/java/petadoption/api/misc/ClearDataController.java
+++ pet-adoption-api/src/main/java/petadoption/api/misc/ClearDataController.java
@@ -11,7 +11,7 @@ import petadoption.api.user.UserService;
  * Just using this for testing.
  */
 
-@CrossOrigin(origins = {"http://localhost:3000","http://35.224.27.57:3000"})
+@CrossOrigin(origins = {"http://localhost:3000","http://34.69.88.79:3000"})
 @RestController
 public class ClearDataController {
 
