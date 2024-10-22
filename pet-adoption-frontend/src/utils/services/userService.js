110e4d6344ac9bfc3b9bce8d899382e08497ec34
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2024-10-21 19:20:09 -0500

    console log apiurl

--- pet-adoption-frontend/src/utils/services/userService.js
+++ pet-adoption-frontend/src/utils/services/userService.js
@@ -78,6 +78,8 @@ const userService = () => {
     };
 
     const registerOwner = async (formData, profilePic) => {
+        console.log('registerOwner function called')
+        console.log(`apiUrl: ${apiUrl}`)
         const response = await fetch(`${apiUrl}/api/owners`, {
             method: "POST",
             headers: {
