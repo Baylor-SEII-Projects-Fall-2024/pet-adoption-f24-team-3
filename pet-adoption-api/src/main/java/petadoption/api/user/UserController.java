857f7955170d6208e92098e093d4c8918ce62c06
Jarpletz <rothpletz5@gmail.com>
2024-10-26 11:35:05 -0500

    Successfully set up basic classes and data structures

--- pet-adoption-api/src/main/java/petadoption/api/user/UserController.java
+++ pet-adoption-api/src/main/java/petadoption/api/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import petadoption.api.recommendations.RecommendationsService;
 import petadoption.api.user.dtos.CenterDto;
 import petadoption.api.user.dtos.LoginDto;
 import petadoption.api.user.dtos.OwnerDto;
