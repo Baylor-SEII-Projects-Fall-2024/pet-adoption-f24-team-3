0b7576bbb282f921c851d5795823c26e7fdf1f1b
SamuelF2 <samueltfries@gmail.com>
2024-11-03 01:38:30 -0500

    Implemented change of state (useEffect) for unread messages, and showing only when > 0. Currently waiting on service layer to exist so I can hook it up...

--- pet-adoption-frontend/src/components/chat/ChatContainer.jsx
+++ pet-adoption-frontend/src/components/chat/ChatContainer.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import ChatIcon from '@mui/icons-material/Chat';
 import CloseIcon from '@mui/icons-material/Close';
 import { Box, IconButton, Stack } from '@mui/material'
@@ -8,6 +8,10 @@ import { useChat } from '@/utils/contexts/chatContext';
 
 export default function ChatContainer(props) {
     const { isChatDialogOpen, openChatDialog, closeChatDialog } = useChat();
+    const [unreadMessages, setUnreadMessages] = useState( 0 /* get unread message count (no service exists btw) */);
+    useEffect(() => {
+        setUnreadMessages(/* Still waiting on the service...*/)
+    },[unreadMessages] )
 
     if (!isChatDialogOpen) {
         return (
@@ -27,6 +31,12 @@ export default function ChatContainer(props) {
                     onClick={openChatDialog}
                 >
                     <ChatIcon sx={{ fontSize: "45px" }} />
+                    <div>
+                        {unreadMessages>0?
+                        <span class="icon-button__badge">
+                            {unreadMessages}
+                        </span> : <></>}
+                    </div>
                 </IconButton>
 
             </Box>
