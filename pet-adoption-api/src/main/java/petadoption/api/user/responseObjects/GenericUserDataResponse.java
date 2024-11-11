6185647bbf9e34314011c5ca6aab0b6c3c9bbb1c
Jarpletz <rothpletz5@gmail.com>
2024-11-11 12:29:08 -0600

    Fixed message bugs and added some styling to the messages

---
+++ pet-adoption-api/src/main/java/petadoption/api/user/responseObjects/GenericUserDataResponse.java
@@ -0,0 +1,8 @@
+package petadoption.api.user.responseObjects;
+
+public class GenericUserDataResponse {
+    public Long id;
+    public String name;
+    public String accountType;
+    public String emailAddress;
+}
--- pet-adoption-frontend/src/pages/centers/index.jsx
+++ pet-adoption-frontend/src/pages/centers/index.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import {
-  Button,
   Card,
   CardContent,
   Stack,
