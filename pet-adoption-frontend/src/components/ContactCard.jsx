ae41262ad74c51b282ea9e33afdfca78d09fe92d
Jarpletz <rothpletz5@gmail.com>
2024-11-14 18:48:53 -0600

    cleanup

--- pet-adoption-frontend/src/components/ContactCard.jsx
+++ pet-adoption-frontend/src/components/ContactCard.jsx
@@ -3,8 +3,7 @@ import { useChat } from "@/utils/contexts/chatContext";
 import { Message } from "@mui/icons-material";
 
 export default function ContactCard(props) {
-    /*  */
-    const { contactee, sender, defaultMessage = "" } = props; /* Passing in sender if needed for later */
+    const { contactee, sender, defaultMessage = "" } = props;
     const { openChatByUser } = useChat();
 
     const handleContact = async (event) => {
@@ -14,8 +13,6 @@ export default function ContactCard(props) {
             return
         }
         openChatByUser(sender, contactee, defaultMessage) /* Opens a chat with contactee as receiver */
-
-
     }
 
     return (
