98d37854cc6e10034a54d197252e007c5875aac6
Jarpletz <rothpletz5@gmail.com>
2024-11-18 23:32:02 -0600

    JWT part 1

--- pet-adoption-api/src/main/java/petadoption/api/user/User.java
+++ pet-adoption-api/src/main/java/petadoption/api/user/User.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.List;
+import java.util.Collection;
 
 @Data
 @Entity
@@ -12,7 +16,7 @@ import lombok.Setter;
 @Inheritance(strategy = InheritanceType.JOINED) // Strategy for inheritance
 @DiscriminatorColumn(name = "user_type") // Discriminator column to identify the type of user
 //@Table(name = "`user`") //Used this for testing
-public class User {
+public class User implements UserDetails {
     public static final String TABLE_NAME = "USERS";
 
     @Id
@@ -30,6 +34,31 @@ public class User {
 
     @Column(name = "PROFILE_PIC_PATH")
     protected String profilePicPath;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+    @Override
+    public String getUsername() {
+        return emailAddress;
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
