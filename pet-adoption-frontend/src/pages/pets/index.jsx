f87a789d8ee2ecd8a6c281c2259b31debc651779
Renzenar <118459019+Renzenar@users.noreply.github.com>
2024-12-10 09:59:12 -0600

    Undoing yarn.lock
    This reverts commit e07c4ad065ca5cf74d8fcfe8c592977e859db75c.

--- pet-adoption-frontend/src/pages/pets/index.jsx
+++ pet-adoption-frontend/src/pages/pets/index.jsx
@@ -284,6 +284,14 @@ export default function PetsPage() {
     await fetchFirstData(filter);
   }
 
+  const checkIfEuthanized = (value) => (euthanizedPetIds.includes(value));
+
+  const handlePetClick = (petId) => {
+    if (!checkIfEuthanized(petId)) {
+      router.push(`/pets/${petId}`);
+    }
+  };
+
   const updateTotalDislikes = async (petId, decrement = false) => {
     try {
       if (!grief) return;
@@ -519,7 +527,7 @@ export default function PetsPage() {
                   {animalData.map((pet) => (
                     <Grid item xs={11} sm={5} md={4} lg={4} key={pet.id}>
                       <Box
-                        onClick={() => router.push(`/pets/${pet.id}`)}
+                        onClick={handlePetClick.bind(null, pet.id)}
                         sx={{ cursor: "pointer" }}
                       >
                         <PetCard
